1be2f86ffcf211b6038a4c462ed590b934d3c2e1
TaynadeCarvalhoAlves <taynaalves11@hotmail.com>
2023-11-23 09:56:15 -0300

    Add project files.

---
+++ CadastrodeAlunos/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastrodeAlunos
+{
+    public class Aluno
+    {
+        public string Nome;
+        public int Idade;
+        public double Nota;
+
+        public Aluno(string nome, int idade, double nota)
+        {
+            Nome = nome;
+            Idade = idade;
+            Nota = nota;
+        }
+    }
+}
---
+++ CadastrodeAlunos/Program.cs
@@ -0,0 +1,16 @@
+﻿using CadastrodeAlunos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Aluno aluno1 = new Aluno("Laura", 15, 8.5);
+
+        Console.WriteLine($"Nome: {aluno1.Nome}, Idade: {aluno1.Idade}, Nota: {aluno1.Nota}");
+    }
+}
